0a36cc8f83e6dae48628d8e2a1286e456aa9eedf
Gaurav Dwivedi <113124154+gauravdwivediofficial@users.noreply.github.com>
2023-01-12 17:57:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let imageElement = document.getElementById("image");
+let warningMessageElement = document.getElementById("warningMessage");
+let imageWidthElement = document.getElementById("imageWidth");
+let imageMaxWidth = 300;
+let imageMinWidth = 100;
+let originalImageWidth = 200;
+let maxWidthWarningMessage = "Too big. Decrease the size of the image.";
+let minWidthWarningMessage = "Can't Visible. Increase the size of the image.";
+imageElement.style.width = originalImageWidth + "px";
+imageWidthElement.textContent = originalImageWidth + "px"
+
+function incrementButton() {
+    if (originalImageWidth >= imageMaxWidth) {
+        warningMessageElement.textContent = maxWidthWarningMessage;
+    } else {
+        originalImageWidth = originalImageWidth + 5;
+        let updatedImageWidth = originalImageWidth + "px";
+        warningMessageElement.textContent = "";
+        imageElement.style.width = updatedImageWidth;
+        imageWidthElement.textContent = updatedImageWidth;
+    }
+}
+
+function decrementButton() {
+    if (originalImageWidth <= imageMinWidth) {
+        warningMessageElement.textContent = minWidthWarningMessage;
+    } else {
+        originalImageWidth = originalImageWidth - 5;
+        let updatedImageWidth = originalImageWidth + "px";
+        warningMessageElement.textContent = "";
+        imageElement.style.width = updatedImageWidth;
+        imageWidthElement.textContent = updatedImageWidth;
+    }
+}
